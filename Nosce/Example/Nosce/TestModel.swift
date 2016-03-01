95611de19d24655089bd952f9c8cfb2db5a3150f
Gabriel Coman <dev.gabriel.coman@gmail.com>
2016-03-01 21:20:22 +0000

    commit

---
+++ Nosce/Example/Nosce/TestModel.swift
@@ -0,0 +1,21 @@
+//
+//  TestModel.swift
+//  Nosce
+//
+//  Created by Gabriel Coman on 01/03/2016.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class TestModel: NSObject {
+    
+    var var1: Int = 0
+    var var2: String! = nil
+    
+    init(var1 v1: Int, var2 v2: String) {
+        super.init()
+        var1 = v1;
+        var2 = v2;
+    }
+}
